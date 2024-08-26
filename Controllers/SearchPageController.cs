7c789450a684bc2c732bca3f9577da2d4bde46e2
Suzane Hamze <suzane.hamze@epinova.no>
2024-08-26 15:41:18 +0200

    Lag SearchPageController med metode Index, som tar in string q som parameter.

---
+++ Controllers/SearchPageController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Suzane.WebApp.Controllers
+{
+    public class SearchPageController : Controller
+    {
+        public IActionResult Index(string q)
+        {
+
+
+            //returnerer visningen med parameteren q    
+            return View((object)q);
+        }
+    }
+}
