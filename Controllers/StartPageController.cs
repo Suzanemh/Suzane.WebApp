326144090210323c2f5c314e1d7c14a5b9ebf34d
Suzane Hamze <suzane.hamze@epinova.no>
2024-08-26 13:20:36 +0200

    Lagt  til Index.chhtml

---
+++ Controllers/StartPageController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Suzane.WebApp.Controllers
+{
+
+    [Route("")]
+    public class StartPageController : Controller
+    {
+        [HttpGet("")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
