118503964a7fba032f5d23f3da412aa82cef6790
Suzane Hamze <suzane.hamze@epinova.no>
2024-08-26 13:43:23 +0200

    Lag Views/Shared_Head.cshtml med innhold <title>@ViewData["Title"] - WebApp</title>

---
+++ Views/Shared/Head.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Suzane.WebApp.Views.Shared
+{
+    public class HeadModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
